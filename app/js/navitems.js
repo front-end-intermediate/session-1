05549295516c73062cc507c141baa527534860bc
DannyBoyNYC <DannyBoyNYC>
2018-05-29 16:39:23 -0400

    done

--- app/js/navitems.js
+++ app/js/navitems.js
@@ -44,12 +44,12 @@ const fakeContent = [
 ]
 
 
-// const inventors = [
-// { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-// { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-// { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-// { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-// { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-// { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-// { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-// ];
+const inventors = [
+{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+{ first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+{ first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+{ first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+{ first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+{ first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+];
